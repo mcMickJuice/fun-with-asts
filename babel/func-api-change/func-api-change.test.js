41800b6d2762b3592448a5ae77db59255623b689
Mike Joyce <mikejoyce19@gmail.com>
2017-07-16 20:32:19 -0500

    updated tests with output prop

--- babel/func-api-change/func-api-change.test.js
+++ babel/func-api-change/func-api-change.test.js
@@ -1,17 +1,18 @@
 const plugin = require('./func-api-change')
 const pluginTester = require('babel-plugin-tester')
-
+ 
 pluginTester({
   plugin,
   tests: {
     'does not change non importantFunc invocations': 'someFunc(a, b, c);',
     'changes importantFunc invocations from ordinal to object - params as references': {
       code: 'importantFunc(a,b,c)',
-      snapshot: true
+      output: `importantFunc({\n  name: a,\n  age: b,\n  location: c\n});
+      `
     },
     'changes importantFunc invocations from ordinal to object - params as literals': {
-      code: 'importantFunc("mike", 32, "mpls")',
-      snapshot: true
+      code: `importantFunc('mike', 32, 'mpls')`,
+      output: `importantFunc({\n  name: 'mike',\n  age: 32,\n  location: 'mpls'\n});`
     }
   }
 })
